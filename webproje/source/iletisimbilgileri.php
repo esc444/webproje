6dfff0fdb063bb802deb7795f0d9b9123f7fa364
Ekrem Selcuk <ekrem.celik4@ogr.sakarya.edu.tr>
2022-05-15 20:18:40 +0300

    aa

--- webproje/source/iletisimbilgileri.php
+++ webproje/source/iletisimbilgileri.php
@@ -8,22 +8,27 @@ function alert($msg) {
         $tel = $_POST['tel'];
         $message = $_POST['message'];
 
-
+        if (strpos($email, "@") != true) {
+            alert("Lütfen geçerli bir email adresi giriniz.");
+            header("Refresh: 0; url=iletisim.html");
+            exit;
+        }
+        
 
        if($name=="" or $email =="" or $tel=="" or $message=="" )
        {
-        alert('Eksik Bilgi Girdiniz.');
-        header("Refresh: 0; url=iletisim.html");
+            alert('Eksik Bilgi Girdiniz.');
+            header("Refresh: 0; url=iletisim.html");
        }
        else {
            
-        alert(" Mesaj isteğiniz alınmıştır : İsim : $name  E-mail : $email Numara : $tel Mesajınız : $message ");
-        $dosya = fopen ("iletisim.txt" , 'w'); //dosya oluşturma işlemi
-$yaz=" İsim : $name  E-mail : $email Numara : $tel Mesajınız : $message "; //dosya içine ne yazmak istiyorsanız buraya yazın. $değer
-fwrite ( $dosya , $yaz ) ;
-fclose ($dosya);
+            alert(" Mesaj isteğiniz alınmıştır : İsim : $name  E-mail : $email Numara : $tel Mesajınız : $message ");
+            $dosya = fopen ("iletisim.txt" , 'w'); //dosya oluşturma işlemi
+            $yaz=" İsim : $name  E-mail : $email Numara : $tel Mesajınız : $message "; //dosya içine ne yazmak istiyorsanız buraya yazın. $değer
+            fwrite ( $dosya , $yaz ) ;
+            fclose ($dosya);
 
-        header("Refresh: 0; url=iletisim.html");
+            header("Refresh: 0; url=iletisim.html");
        }
 }
 
